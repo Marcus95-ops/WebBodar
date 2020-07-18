deafcd6a2d847276cc655a752f180110056c541b
Marcus95-ops <66971161+Marcus95-ops@users.noreply.github.com>
2020-07-18 20:51:58 +0200

    Add files via upload

---
+++ Beräkningsprogram.js
@@ -0,0 +1,16 @@
+
+
+/*function MerÄn(Telenr) {
+    var Telenr = document.getElementsByName("Telnr")
+    if (Telenr >= String([12]))
+        postMessage("Telefonnummer för långt!")
+    else (Telenr <= String([12]))
+        postMessage("Telefonnummer för långt!")
+}
+
+function Beräkning(Bod) {
+    var höjd = document.getElementsByName(höjd), bredd = document.getElementsByName(bredd), djup = document.getElementsByName(djup)
+    var tak = document.getElementsByName()
+    
+}*/
+
